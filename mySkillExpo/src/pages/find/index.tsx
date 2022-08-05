1d1a1d01aaa2092546821eebfa6efc24cb1fe752
RafaelNeiva2 <109110984+RafaelNeiva1@users.noreply.github.com>
2022-08-05 18:09:50 -0300

    second commit

--- mySkillExpo/src/pages/find/index.tsx
+++ mySkillExpo/src/pages/find/index.tsx
@@ -1,13 +1,63 @@
-import React from "react";
+import React, { useEffect,useState } from "react";
 import { View,Text, TextInput } from "react-native";
 import styles from "./style";
 import AntDesign from "@expo/vector-icons/AntDesign";
+import SQL from "../../assets/image/postgresql.png";
 
 export default function Find(){
+    interface Linguagens {
+        id: number,
+        nome: string,
+        url: string
+    }
+
+    const [searchText, setSearchText] = useState('');
+
+
+    const [linguagens, setLinguagens] = useState([
+        {
+            id:'1',
+            nome:'SQL',
+            url : SQL
+        }
+
+
+
+
+    ]);
+
+    const [list, setList] = useState(linguagens);
+
+    // useEffect(() => {
+
+    //     buscaListaLinguagens().then((res) => {
+    //         setProdutos(res.data);
+    //     }).catch((err) => {
+    //         console.log(err);
+    //     }).finally(() => {
+
+    //     })
+    // }, [])
+
+    useEffect(() => {
+        if (searchText === '') {
+            setList(linguagens);
+        } else {
+            setList(
+                linguagens.filter(
+                    (item) =>
+                        item.nome.toLowerCase().indexOf(searchText.toLowerCase()) > -1
+                )
+            );
+        }
+    }, [searchText]);
+
+
     return(
         <View style={styles.container}>
             <TextInput placeholder="Search" style={styles.input} />
             <View style={styles.card}>
+                
 
             </View>
         </View>
